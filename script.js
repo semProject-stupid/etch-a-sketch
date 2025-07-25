d917cfc74b37d9af438a3bcf01d131b69a17672c
semProject-stupid <try.another.son@gmail.com>
2025-07-26 03:13:32 +0500

    Set up new working grid

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ const parentContainer = document.querySelector(".container");
 const bodyContainer = document.querySelector("body");
 
 //setting up the grid
-let userChoice = prompt ("How many grids do you want in your sketchpad?");
-while(userChoice > 1000 || userChoice <= 0) {
-    alert("Sorry, the number of grids has to be between 1 and 1000");
-    userChoice = prompt ("How many grids do you want in your sketchpad? (max 1000)");
+let userChoice = prompt ("Enter the size of grid you would like in your sketchpad: (min 1 and max 100)");
+while(userChoice > 100 || userChoice <= 0) {
+    alert("Sorry, the number of grids has to be between 1 and 100");
+    userChoice = prompt ("Enter the size of grid you would like in your sketchpad: (min 1 and max 100");
 }
 
-bodyContainer.style.width = (userChoice * 0.5) + "px";
-bodyContainer.style.height = (userChoice * 0.5) + "px";
+let totalSquares = Math.floor(600 / userChoice);
 
-for (let i = 0; i < userChoice; i++) {
+for (let i = 0; i < totalSquares * totalSquares; i++) {
     const gridDiv = document.createElement("div");
     gridDiv.classList = "grid";
+    gridDiv.style.height = userChoice + "px";
+    gridDiv.style.width = userChoice + "px";
     parentContainer.appendChild(gridDiv);
+
+    //adding hover effect
+    gridDiv.addEventListener("mouseenter", () => {
+    gridDiv.style.backgroundColor = "black";
+})
 }
+
