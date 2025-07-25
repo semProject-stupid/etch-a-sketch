7befa351f753f8c688883ed2c75fbdcc01720dc7
semProject-stupid <try.another.son@gmail.com>
2025-07-26 03:35:59 +0500

    Set up reset button

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 const parentContainer = document.querySelector(".container");
 const bodyContainer = document.querySelector("body");
+const resetButton = document.querySelector("#reset");
+const newButton = document.querySelector("#new");
 
-//setting up the grid
-let userChoice = prompt ("Enter the size of grid you would like in your sketchpad: (min 1 and max 100)");
-while(userChoice > 100 || userChoice <= 0) {
-    alert("Sorry, the number of grids has to be between 1 and 100");
-    userChoice = prompt ("Enter the size of grid you would like in your sketchpad: (min 1 and max 100");
+function deleteGrids() {
+    
 }
 
-let totalSquares = Math.floor(600 / userChoice);
+function createNew() {
+    //setting up the grid
+    let userChoice = prompt ("Enter the size of grid you would like in your sketchpad: (min 1 and max 100)");
+    while(userChoice > 100 || userChoice <= 0) {
+        alert("Sorry, the number of grids has to be between 1 and 100");
+        userChoice = prompt ("Enter the size of grid you would like in your sketchpad: (min 1 and max 100");
+    }
 
+    let totalSquares = Math.floor(600 / userChoice);
+
+    for (let i = 0; i < totalSquares * totalSquares; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.classList = "grid";
+        gridDiv.style.height = userChoice + "px";
+        gridDiv.style.width = userChoice + "px";
+        parentContainer.appendChild(gridDiv);
+
+        //adding hover effect
+        gridDiv.addEventListener("mouseenter", () => {
+        gridDiv.style.backgroundColor = "black";
+        })
+    }
+}
+
+
+//clearing existing sketchpad
+function resetGrid() {
+    const newGridDivs = document.querySelectorAll(".grid");
+    newGridDivs.forEach(grid => {
+        grid.style.backgroundColor = "white";
+    })
+}
+
+//event listeners
+resetButton.addEventListener("click", () => resetGrid());
+newButton.addEventListener("click", createNew);
+
+
+/* personal:
 for (let i = 0; i < totalSquares * totalSquares; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList = "grid";
-    gridDiv.style.height = userChoice + "px";
-    gridDiv.style.width = userChoice + "px";
-    parentContainer.appendChild(gridDiv);
+        newGridDivs[i].style.backgroundColor = "white";
+} //rmbr it returns nodelist, so have to use [i]
 
-    //adding hover effect
-    gridDiv.addEventListener("mouseenter", () => {
-    gridDiv.style.backgroundColor = "black";
-})
-}
+*/
 
